f0cdf1d8486d05e0a69c5fff2a3388c41cd237b9
Lee <leekinyuakabiru@gmail.com>
2025-07-10 12:24:34 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Home() {
+function Home({ username, city }) {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        (`${username}, is a Web Developer from, ${city}`)
       </h1>
     </div>
   );
 }
+
+export default Home;
